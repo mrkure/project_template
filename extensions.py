f90d15e7d4391ea7bef03f0aa95ea0551ee2863c
Zdenek Carda <caz2bj@bosch.com>
2026-01-27 09:04:47 +0100

    gjhk

--- extensions.py
+++ extensions.py
@@ -1,5 +1,6 @@
 
 import os
+import subprocess
 from jinja2.ext import Extension
 from pathlib import Path
 import os
@@ -7,6 +8,12 @@ import os
 def user_name() -> str:
     return os.getlogin().lower()
 
+def git_user_name(default: str) -> str:
+    return subprocess.getoutput("git config user.name").strip() or default
+
+
+def git_user_email(default: str) -> str:
+    return subprocess.getoutput("git config user.email").strip() or default
 
 def slugify(value):
     return value.lower().replace(" ", "_").replace("-", "_")
@@ -15,7 +22,8 @@ class MyExtension(Extension):
     def __init__(self, environment):
         super().__init__(environment)
         environment.globals["user_name"] = user_name
-
+        environment.globals["git_user_name"] = git_user_name
+        environment.globals["git_user_email"] = git_user_email
 
 class SlugifyExtension(Extension):
     def __init__(self, environment):
